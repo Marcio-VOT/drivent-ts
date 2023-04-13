ed76c2002798685416c0aca49521007f931946cf
Marcio-VOT <marciofsantos65@gmail.com>
2023-04-13 00:22:31 -0300

    stash

--- src/services/payments-service/index.ts
+++ src/services/payments-service/index.ts
@@ -3,14 +3,11 @@ import paymentRepository from '@/repositories/payment-repository';
 
 async function getTicketPaymentInfo({ ticketId, userId }: { ticketId: string; userId: number }) {
   const ticket = await paymentRepository.findTicket(ticketId);
-  // console.log({ userId, UserTiket, allTickets, ticketId });
-
   if (!ticket) throw notFoundError();
 
   const {
     Ticket: [UserTiket],
   } = await paymentRepository.findTicketOwner({ ticketId, userId });
-
   if (!UserTiket) throw unauthorizedError();
 
   const ticketInfo = await paymentRepository.findFirst(ticketId);
