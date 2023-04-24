90baf9f7a99bad5e7352c8fb6a1c1b8a3a8acc48
Marcio-VOT <marciofsantos65@gmail.com>
2023-04-24 02:34:54 -0300

    fix: wrong name to schema export

--- src/schemas/tickets-schemas.ts
+++ src/schemas/tickets-schemas.ts
@@ -1,6 +1,5 @@
 import Joi from 'joi';
-import { InputTicketBody } from '@/protocols';
 
-export const ticketsSchema = Joi.object<InputTicketBody>({
+export const ticketTypeIdValidation = Joi.object({
   ticketTypeId: Joi.number().required(),
 });
